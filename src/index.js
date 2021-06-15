78f9dd48baee4c346e37af13fa77d8fa83c2d149
Mr-iMikeyyy <56235937+Mr-iMikeyyy@users.noreply.github.com>
2021-06-15 08:53:39 -0400

    condensend map function down to fat arrow

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import emojipedia from "./emojipedia";
 
-function meaning(emojiEntry) {
-  return (emojiEntry = emojiEntry.meaning.substring(0, 100));
-}
+// function meaning(emojiEntry) {
+//   return emojiEntry.meaning.substring(0, 100);
+// }
 
-const newEmojipedia = emojipedia.map(meaning);
+// const newEmojipedia = emojipedia.map(meaning);
+
+const newEmojipedia = emojipedia.map((emojiEntry) =>
+  emojiEntry.meaning.substring(0, 100)
+);
+
 console.log(newEmojipedia);
